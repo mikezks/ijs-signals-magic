43c0a8aa5241b37f57dddfeff4395bf20360fd6c
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-25 16:11:06 +0200

    feat: live coding - computed, effect - microtask, task - glitch-free - removed signal reference

--- projects/shell/src/app/booking/flight/features/search/search.component.ts
+++ projects/shell/src/app/booking/flight/features/search/search.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, DatePipe, JsonPipe, NgFor, NgIf } from '@angular/common';
-import { Component, Signal, WritableSignal, computed, effect, signal } from '@angular/core';
+import { Component, WritableSignal, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { injectBookingFeature } from '../../../+state/booking.state';
@@ -10,8 +10,8 @@ import { FlightFilterComponent } from "../../ui/flight-filter/flight-filter.comp
 @Component({
   selector: 'app-flight-search',
   standalone: true,
-  // template: '<h2>No Reactive View active!</h2>',
-  templateUrl: './search.component.html',
+  template: '<h2>No Reactive View active!</h2>',
+  // templateUrl: './search.component.html',
   imports: [
     NgIf, NgFor, DatePipe, JsonPipe, AsyncPipe,
     RouterLink,
@@ -30,4 +30,50 @@ export class SearchComponent {
     3: true,
     5: true,
   };
+
+  protected flightRoute = computed(
+    () => 'From ' + this.from() + ' to ' + this.to() + '.'
+  );
+
+  constructor() {
+    effect(
+      () => console.log(this.flightRoute())
+    );
+
+    queueMicrotask(() => {
+      console.log('%c MicroTask 1: Set From to "Basel"', 'color: yellow')
+      this.from.set('Basel');
+      console.log('%c MicroTask 1: Set From to "London"', 'color: yellow')
+      this.from.set('London');
+      queueMicrotask(() => {
+        console.log('%c MicroTask 2: Set From to "Rome"', 'color: yellow')
+        this.from.set('Rome');
+      });
+
+      setTimeout(() => {
+        console.log('%c 3s: Set From to "Barcelona"', 'color: green')
+        this.from.set('Barcelona');
+      }, 3_000);
+      setTimeout(() => {
+        console.log('%c 5s: Set From to "Madrid"', 'color: green')
+        this.from.set('Madrid')
+      }, 5_000);
+      setTimeout(() => {
+        console.log('%c 8s: Set FlightRoute Signal to null', 'color: green')
+        this.flightRoute = null as any as WritableSignal<string>;
+      }, 8_000);
+      setTimeout(() => {
+        console.log('%c 11s: Set FlightRoute to Signal w/ value "Hacked! >:-("', 'color: green')
+        this.flightRoute = signal('Hacked! >:-(');
+      }, 11_000);
+      setTimeout(() => {
+        console.log('%c 14s: Define a new unrelated Signal"', 'color: green')
+        signal('Unrelated Signal');
+      }, 14_000);
+      setTimeout(() => {
+        console.log('%c 17s: Set From to "Oslo"', 'color: green')
+        this.from.set('Set From to "Oslo"');
+      }, 17_000);
+    });
+  }
 }
